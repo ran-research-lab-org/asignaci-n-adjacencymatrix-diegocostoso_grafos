fc645789812787f1867a56910d70f9ff8cdb1f11
DiegoCostoso <diego.costoso@upr.edu>
2025-11-05 19:24:33 +0000

    final

--- main.cpp
+++ main.cpp
@@ -9,11 +9,19 @@ using namespace std;
 
 int main() {
   Graph G(10);
-  G.addEdge(0,9);
+  G.addEdge(0, 9);
   G.addEdge(4, 2);
+  G.addEdge(4, 3);
+  G.addEdge(1, 3);
+  G.addEdge(2, 3);
+  G.addEdge(3, 4);
+  G.addEdge(7, 4);
+  G.addEdge(6, 4);
+
 
   G.print();
+  std::cout << std::boolalpha;
   std::cout << "\nCantidad de Aristas: " << G.numEdges() << std::endl;
-  std::cout << "In-degree del vertice: " << G.inDegree(9) << std::endl;
-  std::cout << "u tiene el mayor In-degree? : " << G.isInfluencer(2) << std::endl;
+  std::cout << "In-degree del vertice: " << G.inDegree(3) << std::endl;
+  std::cout << "u tiene el mayor In-degree? : " << G.isInfluencer(4) << std::endl;
 }
